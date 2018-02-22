20788a90438b6104df88d62a1ee323fc0792eb2f
UCTRONICS <avahe@uctronics.com>
2018-02-22 17:00:56 +0800

    add RPI_Motorized_IRCut_Control

---
+++ CameraLED.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import os, fcntl, struct
+# RaspberryPi 3 camera LED controller by BigNerd95
+class CameraLED():
+
+    IOCTL_MAILBOX = 0xC0046400
+    REQUEST_SUCCESS = 0x80000000
+    SET_TAG = 0x38041
+    GET_TAG = 0x30041
+
+    def __init__(self, led=134):
+        self.__vcio = os.open("/dev/vcio", os.O_TRUNC)
+        self.__led = led
+    
+    def __firmware_request__(self, tag, state = 0):
+        buffer = struct.pack("=8I", # buffer format, int32 buffer[8] (native endian)
+                32,                 # total message length in bytes
+                0, 
+                tag,                # tag
+                8,                  # tag data size
+                0,
+                self.__led,         # tag data: led number
+                state,              # tag data: state
+                0
+            )
+        # send request
+        result = fcntl.ioctl(self.__vcio, self.IOCTL_MAILBOX, buffer, True)
+
+        _, success, _, _, _, _, newState, _ = struct.unpack("=8I", result)
+
+        if success == self.REQUEST_SUCCESS:
+            return newState
+        else:
+            raise Exception("RPi firmware error!")
+
+    def on(self):
+        return self.__firmware_request__(self.SET_TAG, 1)
+
+    def off(self):
+        return self.__firmware_request__(self.SET_TAG, 0)
+
+    def toggle(self):
+        if self.state() == 1:
+            return self.off()
+        else:
+            return self.on()
+
+    def state(self):
+        return self.__firmware_request__(self.GET_TAG)
+
+if __name__ == "__main__":
+    import sys
+
+    def usage():
+        print("Usage:", sys.argv[0], "(state|toggle|on|off)")
+    
+    if len(sys.argv) == 2:
+        led = CameraLED()
+        if sys.argv[1] == "state":
+            print("State:", led.state())
+        elif sys.argv[1] == "toggle":
+            print("State:", led.toggle())
+        elif sys.argv[1] == "on":
+            print("State:", led.on())
+        elif sys.argv[1] == "off":
+            print("State:", led.off())
+        else:
+            usage()
+    else:
+        usage() 
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# RPI_Motorized_IRCut_Control
-It is a repository for controlling the motorized IRcut filter of Raspberry pi camera
+# CameraLED
+Pure python3 library to control Camera LED for RaspberryPi 3
+
+# Background
+This library is used for ArduCAM with motorized IR Cut Filter.  
+The IR Cur filter is connected to the camera LED, so by controlling the LED you can control the IR filter.  
+This python library is used to control the camera LED (and IR filter) on Raspberry 3.  .  
+
+# Prerequisites
+You have to disable the automatic management of camera led in `/boot/config.txt`.  
+```bash
+$ sudo echo "disable_camera_led=1" >> /boot/config.txt
+$ reboot
+```
+# Installation
+```bash
+$ wget https://raw.githubusercontent.com/arducam/RPI_Motorized_IRCut_Control/master/CameraLED.py 
+$ chmod 755 CameraLED.py
+```
+
+# Usage
+You need to be root (or at least be able to use sudo).
+### Standalone
+You can use the library as a standalone command:
+```bash
+$ sudo ./CameraLED.py 
+Usage: ./CameraLED.py (state|toggle|on|off)
+
+$ sudo ./CameraLED.py state
+State: 0
+
+$ sudo ./CameraLED.py toggle
+State: 1
+
+$ sudo ./CameraLED.py toggle
+State: 0
+
+$ sudo ./CameraLED.py on    
+State: 1
+
+$ sudo ./CameraLED.py off
+State: 0
+```
+### Python library
+You can use my library in your Camera projects
+```python
+from CameraLED import CameraLED
+led = CameraLED() # CameraLED(134)
+led.on()
+led.off()
+led.toggle()
+led.state()
+```
+
+# Photos
+LED OFF = NO IR filter = Night vision  
+![image](doc/led_off.jpg)  
+LED ON = IR filter = Daily vision  
+![image](doc/led_on.jpg)  
